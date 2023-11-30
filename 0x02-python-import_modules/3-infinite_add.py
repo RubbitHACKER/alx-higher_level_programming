8abe683ee7ce517d1cb67e04aa3d7e7b0fff9fc5
RubbitHacker <hajjimohamedreda1@gmail.com>
2023-11-30 21:43:37 +0100

    h

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    import sys, math
-    result = 0
-    for i in sys.argv:
-        result += int(i)
-        print("{}".format(result))
+    args = sys.argv[1:]
+    
+    if args:
+        result = sum(int(arg) for arg in args)
+        print(result)
+    else:
+        print(0)
